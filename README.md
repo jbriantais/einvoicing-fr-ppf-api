6ae5db5f8657d7bbb3fe00b857c3a508b7971b06
Johan Briantais <contact@briantais.net>
2023-08-03 23:40:47 +0000

    First commit of the sources

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # einvoicing-fr-ppf-api
 REST API documentation of e-invoicing public portal (PPF) for future CTC requirements in France
+
+## About this repository
+This repository contains tools for extracting OpenAPI specifications from the [official specifications of the PPF](https://www.impots.gouv.fr/specifications-externes-b2b) and deploying a Swagger UI documentation site to GitHub pages.
+
+## Author
+[Johan Briantais](https://github.com/jbriantais)
---
+++ build-swagger-site.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -Eeuxo pipefail
+# This script downloads a ZIP file containing OpenAPI specs, extracts them and generates a site with Swagger UI
+
+echo "Create output directory for GitHub Pages content"
+mkdir "$OUTPUT_DIR"
+
+echo "Copy the html page containing Swagger UI"
+cp index.html "$OUTPUT_DIR"
+
+echo "Download the ZIP file containing the OpenAPI specs"
+curl -sSLo specs.zip "$SPEC_ZIP_URL"
+
+echo "Extract OpenAPI specs"
+unzip -j specs.zip "$SPEC_EXTRACT_FILTER" -d "$OUTPUT_DIR/specs"
+
+echo "List OpenAPI spec files in specs directory and Generate Swagger configuration file in YAML containing the relative URLs"
+(
+    echo 'urls:'
+    ls -1 "$OUTPUT_DIR/specs" | sed -ne 's|^\(.*\)\.\([^.]*\)$|  - url: "specs/\1.\2"\n    name: "\1"|p'
+) > _site/generated-swagger-config.yaml
